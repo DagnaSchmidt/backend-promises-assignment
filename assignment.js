62619a8d6351fbeb0416b18885a4c40513b6dd08
Dagna Schmidt <dagdagi889@gmail.com>
2024-10-07 19:12:56 +0200

    second part

--- assignment.js
+++ assignment.js
@@ -9,49 +9,40 @@ const dice = () => {
     });
 };
 
-const fetchAdviceById = (id) => {
-    fetch(`https://api.adviceslip.com/advice/${id}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Response status is not OK');
-            } else {
-                return response.json();
-            }
-        })
-        .then((data) => {
-            const advice = data.slip.advice;
-            console.log(advice);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+const fetchAdviceById = async (id) => {
+    try {
+        const result = await fetch(`https://api.adviceslip.com/advice/${id}`);
+        const data = await result.json();
+        const advice = data.slip.advice;
+        console.log(advice);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const fetchJoke = () => {
-    fetch('https://official-joke-api.appspot.com/random_joke')
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Response status is not OK');
-            } else {
-                return response.json();
-            }
-        })
-        .then((data) => {
-            const question = data.setup;
-            const answer = data.punchline;
-            console.log(question + ' ' + answer);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+const fetchJoke = async () => {
+    try {
+        const result = await fetch('https://official-joke-api.appspot.com/random_joke');
+        const data = await result.json();
+        const question = data.setup;
+        const answer = data.punchline;
+        console.log(question + ' ' + answer);
+    } catch (error) {
+        console.log(error);
+
+    }
 };
 
-dice()
-    .then(() => {
+const rollTheDice = async () => {
+    try {
+        const outcome = await dice();
         fetchAdviceById(30);
-    })
-    .catch(() => {
+    } catch (error) {
         fetchJoke();
-    });
+    }
+};
+
+rollTheDice();
 
 //you get advice only if dice is 6
+//advice: 'When in doubt, just take the next small step.'
